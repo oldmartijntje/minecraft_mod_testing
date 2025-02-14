e7cf906d8c8c7d09a1a15baa6c30a2d23aaee739
Martijn <oldmartijntje@gmail.com>
2025-02-14 12:09:14 +0100

    fixed the problem and added the item to minecraft

---
+++ avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/TutorialModelClient.java
@@ -0,0 +1,10 @@
+package com.oldmartijntje.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModelClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/item/ModItems.java
@@ -0,0 +1,24 @@
+package com.oldmartijntje.tutorialmod.item;
+
+import com.oldmartijntje.tutorialmod.AvansEetBanaantjes;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(AvansEetBanaantjes.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        AvansEetBanaantjes.LOGGER.info("registering ModItems for " + AvansEetBanaantjes.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        });
+    }
+ }
