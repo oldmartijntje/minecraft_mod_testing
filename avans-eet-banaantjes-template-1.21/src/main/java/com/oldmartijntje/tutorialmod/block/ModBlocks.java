cbdacecfc9c41f37afb2bc9a38adb1f4d17cd562
Martijn <oldmartijntje@gmail.com>
2025-02-20 18:11:27 +0100

    blocks drop items now :3 \n episode 5

--- avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/block/ModBlocks.java
+++ avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import com.oldmartijntje.tutorialmod.AvansEetBanaantjes;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,6 +12,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
@@ -23,6 +25,17 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(3f)
                     .requiresTool()
                     .sounds(BlockSoundGroup.AMETHYST_CLUSTER)));
+
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f)
+                    .requiresTool()));
+
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f)
+                    .sounds(BlockSoundGroup.DEEPSLATE)
+                    .requiresTool()));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
