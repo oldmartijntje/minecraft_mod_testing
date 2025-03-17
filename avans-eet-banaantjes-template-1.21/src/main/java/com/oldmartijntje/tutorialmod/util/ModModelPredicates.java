dd3abb44433e5cfd0d5427e97ca3cce821fb202c
Martijn <oldmartijntje@gmail.com>
2025-03-17 16:06:28 +0100

    Custom Bow [ep 22]

--- avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/util/ModModelPredicates.java
+++ avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/util/ModModelPredicates.java
@@ -4,12 +4,29 @@ import com.oldmartijntje.tutorialmod.AvansEetBanaantjes;
 import com.oldmartijntje.tutorialmod.component.ModDataComponentTypes;
 import com.oldmartijntje.tutorialmod.item.ModItems;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
 public class ModModelPredicates {
     public static void registerModelPredicates() {
         ModelPredicateProviderRegistry.register(ModItems.CHISEL, Identifier.of(AvansEetBanaantjes.MOD_ID, "used"),
                 (stack, world, entity, seed) -> stack.get(ModDataComponentTypes.COORDINATES) != null ? 1f : 0f);
+        registerCustomBow(ModItems.KAUPEN_BOW);
+    }
 
+    private static void registerCustomBow(Item item) {
+        ModelPredicateProviderRegistry.register(item, Identifier.ofVanilla("pull"), (stack, world, entity, seed) -> {
+            if (entity == null) {
+                return 0.0F;
+            } else {
+                return entity.getActiveItem() != stack ? 0.0F : (float)(stack.getMaxUseTime(entity) - entity.getItemUseTimeLeft()) / 20.0F;
+            }
+        });
+        ModelPredicateProviderRegistry.register(
+                item,
+                Identifier.ofVanilla("pulling"),
+                (stack, world, entity, seed) -> entity != null && entity.isUsingItem() && entity.getActiveItem() == stack ? 1.0F : 0.0F
+        );
     }
 }
