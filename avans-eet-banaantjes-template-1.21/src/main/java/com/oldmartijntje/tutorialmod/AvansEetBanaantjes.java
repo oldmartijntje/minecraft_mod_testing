cfa69534a90d1f2f305d356f89b107c0787f76eb
Martijn <oldmartijntje@gmail.com>
2025-03-17 16:24:04 +0100

    Events [ep 23]

--- avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/AvansEetBanaantjes.java
+++ avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/AvansEetBanaantjes.java
@@ -7,8 +7,15 @@ import com.oldmartijntje.tutorialmod.item.ModItems;
 import com.oldmartijntje.tutorialmod.util.HammerUsageEvent;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,5 +35,16 @@ public class AvansEetBanaantjes implements ModInitializer {
 		FuelRegistry.INSTANCE.add(ModItems.FALLEN_STAR, 600);
 
 		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+
+		AttackEntityCallback.EVENT.register((playerEntity, world, hand, entity, entityHitResult) -> {
+			if(entity instanceof SheepEntity sheepEntity && !world.isClient()) {
+				if (playerEntity.getMainHandStack().getItem() == Items.END_ROD) {
+					playerEntity.sendMessage(Text.literal("Bababooi!"));
+					playerEntity.getMainHandStack().decrement(1);
+					sheepEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 600, 6));
+				}
+			}
+			return ActionResult.PASS;
+		});
 	}
 }
--- avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/AvansEetBanaantjesClient.java
+++ avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/AvansEetBanaantjesClient.java
@@ -6,7 +6,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 
-public class TutorialModelClient implements ClientModInitializer {
+public class AvansEetBanaantjesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_DOOR, RenderLayer.getCutout());
