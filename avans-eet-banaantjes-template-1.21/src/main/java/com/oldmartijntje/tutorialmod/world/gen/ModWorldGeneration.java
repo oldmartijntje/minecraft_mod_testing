cf1c0772adb4f8d3178b9809d67325717f9f77b2
Martijn <oldmartijntje@gmail.com>
2025-04-24 17:17:40 +0200

    Tree generation [ep 36]

--- avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/world/gen/ModWorldGeneration.java
+++ avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/world/gen/ModWorldGeneration.java
@@ -2,6 +2,9 @@ package com.oldmartijntje.tutorialmod.world.gen;
 
 public class ModWorldGeneration {
     public static void generateModWorldGen() {
+        // order might matter.
         ModOreGeneration.generateOres();
+
+        ModTreeGeneration.generateTrees();
     }
 }
