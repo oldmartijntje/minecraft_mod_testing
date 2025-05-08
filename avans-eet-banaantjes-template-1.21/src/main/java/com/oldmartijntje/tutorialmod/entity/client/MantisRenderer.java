0411bd19aafb1c5c2eb966758c432d45a900ba80
OldMartijntje_Frituurpan <oldmartijntje@gmail.com>
2025-05-08 19:06:29 +0200

    entity variants [ep 40]

--- avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/entity/client/MantisRenderer.java
+++ avans-eet-banaantjes-template-1.21/src/main/java/com/oldmartijntje/tutorialmod/entity/client/MantisRenderer.java
@@ -1,21 +1,35 @@
 package com.oldmartijntje.tutorialmod.entity.client;
 
+import com.google.common.collect.Maps;
 import com.oldmartijntje.tutorialmod.AvansEetBanaantjes;
+import com.oldmartijntje.tutorialmod.entity.client.MantisModel;
 import com.oldmartijntje.tutorialmod.entity.custom.MantisEntity;
+import com.oldmartijntje.tutorialmod.entity.custom.MantisVariant;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+
+import java.util.Map;
 
 public class MantisRenderer extends MobEntityRenderer<MantisEntity, MantisModel<MantisEntity>> {
+    private static final Map<MantisVariant, Identifier> LOCATION_BY_VARIANT =
+            Util.make(Maps.newEnumMap(MantisVariant.class), map -> {
+                map.put(MantisVariant.DEFAULT,
+                        Identifier.of(AvansEetBanaantjes.MOD_ID, "textures/entity/mantis/mantis.png"));
+                map.put(MantisVariant.ORCHID,
+                        Identifier.of(AvansEetBanaantjes.MOD_ID, "textures/entity/mantis/mantis_orchid.png"));
+            });
+
     public MantisRenderer(EntityRendererFactory.Context context) {
         super(context, new MantisModel<>(context.getPart(MantisModel.MANTIS)), 0.75f);
     }
 
     @Override
     public Identifier getTexture(MantisEntity entity) {
-        return Identifier.of(AvansEetBanaantjes.MOD_ID, "textures/entity/mantis/mantis.png");
+        return LOCATION_BY_VARIANT.get(entity.getVariant());
     }
 
     @Override
